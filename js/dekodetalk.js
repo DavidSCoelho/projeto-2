d08ccd1053370404c13058377d4d4f4fc462c006
DavidSCoelho <113794639+DavidSCoelho@users.noreply.github.com>
2023-03-15 12:53:16 -0300

    Adiciona Versão corrigida do Projeto 2

--- js/dekodetalk.js
+++ js/dekodetalk.js
@@ -1,66 +1,106 @@
-var codigo = document.getElementById('code')
+var code = document.getElementById('code')
 var copia = document.querySelector('.texto')
-var opcao = document.querySelector('#opt')
-let codificar = document.querySelector('#encode')
-let decodificar = document.querySelector('#decode')
-var retorno = document.querySelector('#texto')
-var alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+var opt = document.querySelector('#opt')
+let encode = document.querySelector('#encode')
+let decode = document.querySelector('#decode')
+var saida = document.querySelector('#texto')
 
-function cesar(){
-var conteudo = copia.value
-var uppercut = conteudo.toUpperCase()
-var formula = (number(opcao.value)%26)
-var volta = ""
-for (var d = 0; d < uppercut.length; d++) {
-   for(var e = 0; e <alfabeto.length; e++ ); {
-    if(uppercut[d] == alfabeto[e]){
-         volta += alfabeto[e + formula];
-         break;
-    } else if(uppercut[d] == ' ') {
-       volta +=' ';
-       break;
-    } 
+//funções de codificação
+const codeB64 = () =>{
+   return btoa(copia.value)
+};
+const decodeB64 = () => {
+   return atob(copia.value)
+};
+
+const codeCaesarShift = () => {
+   var str = copia.value
+   var incremento = parseInt(opt.value)
+   if (incremento < 0) {
+      return codeCaesarShift(str, incremento + 26);
     }
-   } 
-   return volta  
-}
+ var output = "";
 
-function dCesar(){
-    var conteudo = copia.value
-    var uppercut = conteudo.toUpperCase()
-    var formula = (number(opcao.value)%26)
-    var volta = ""
-    for (var d = 0; d < uppercut.length; d++) {
-       for(var e =uppercut.length; e >=0; e-- ); {
-        if(uppercut[d] == alfabeto[e]){
-             volta += alfabeto[e - formula];
-             break;
-        } else if(uppercut[d] == ' ') {
-           volta +=' ';
-           break;
-        } 
-        }
-       } 
-       return volta  
-    }
-    
-
-codificar.addEventListener('click', function(e){
-    e.preventDefault()
-   if(codigo.value == "base64"){
-   retorno.value = btoa(copia.value);
-
-}else if(codigo.value == "cifra"){
-  retorno.value = cesar()
-}
-})
-
-decodificar.addEventListener('click', function(e){
-    e.preventDefault()
-    if(codigo.value == "base64"){
-    retorno.value = atob(copia.value);
+  for (var i = 0; i < str.length; i++) {
  
- }else if(codigo.value == "cifra"){
-   retorno.value = dCesar()
+   var c = str[i];
+
+    if (c.match(/[a-z]/i)) {
+ 
+     var code = str.charCodeAt(i);
+
+     // Letras Maiúsculas
+     if (code >= 65 && code <= 90) {
+       c = String.fromCharCode(((code - 65 + incremento) % 26) + 65);
+     }
+
+     // Letras Minúsculas
+     else if (code >= 97 && code <= 122) {
+       c = String.fromCharCode(((code - 97 + incremento) % 26) + 97);
+     }
+   }
+
+   // Letras Alteradas
+   output += c;
  }
- })
+
+ // Saída
+ return output;
+};
+
+const decodeCaesarShift = () => {
+   var str = copia.value
+   var incremento = parseInt(opt.value)
+   if (incremento < 0) {
+      return decodeCaesarShift(str, incremento + 26);
+    }
+ var output = "";
+
+  for (var i = 0; i < str.length; i++) {
+ 
+   var c = str[i];
+
+    if (c.match(/[a-z]/i)) {
+ 
+     var code = str.charCodeAt(i);
+
+     // Letras Maiúsculas
+     if (code >= 65 && code <= 90) {
+       c = String.fromCharCode(((code - 65 - incremento + 26) % 26) + 65);
+     }
+
+     // Letras Minúsculas
+     else if (code >= 97 && code <= 122) {
+       c = String.fromCharCode(((code - 97 - incremento + 26) % 26) + 97);
+     }
+   }
+
+   // Letras Alteradas
+   output += c;
+ }
+
+ // Saída
+ return output;
+};
+
+
+//botões finais
+encode.addEventListener('click', function(e){
+   e.preventDefault()
+if (code.value === "base64"){
+   saida.innerText = codeB64()
+} 
+else if(code.value === "cifra"){
+   saida.innerText = codeCaesarShift()
+}
+});
+
+decode.addEventListener('click', function(e){
+   e.preventDefault()
+if (code.value === "base64"){
+   saida.innerText = decodeB64()
+} 
+else if(code.value === "cifra"){
+   saida.innerText = decodeCaesarShift()
+}
+});
